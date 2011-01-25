2889cd6fa3b905d0ccc5271f3a07f9e9095404a0
Jason Staten <jstaten07@gmail.com>
2011-01-24 18:16:59 -0700

    initial commit

---
+++ Fleck.WinFormsExample/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Fleck.WinFormsExample
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtOutput = new System.Windows.Forms.TextBox();
+			this.lbxInput = new System.Windows.Forms.ListBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.btnSend = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtOutput
+			// 
+			this.txtOutput.Location = new System.Drawing.Point(6, 19);
+			this.txtOutput.Name = "txtOutput";
+			this.txtOutput.Size = new System.Drawing.Size(248, 20);
+			this.txtOutput.TabIndex = 1;
+			// 
+			// lbxInput
+			// 
+			this.lbxInput.FormattingEnabled = true;
+			this.lbxInput.Location = new System.Drawing.Point(6, 19);
+			this.lbxInput.Name = "lbxInput";
+			this.lbxInput.Size = new System.Drawing.Size(248, 108);
+			this.lbxInput.TabIndex = 2;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.lbxInput);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(260, 142);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Input";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.btnSend);
+			this.groupBox2.Controls.Add(this.txtOutput);
+			this.groupBox2.Location = new System.Drawing.Point(12, 160);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(260, 90);
+			this.groupBox2.TabIndex = 4;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Output";
+			// 
+			// btnSend
+			// 
+			this.btnSend.Location = new System.Drawing.Point(168, 46);
+			this.btnSend.Name = "btnSend";
+			this.btnSend.Size = new System.Drawing.Size(75, 23);
+			this.btnSend.TabIndex = 2;
+			this.btnSend.Text = "Send!";
+			this.btnSend.UseVisualStyleBackColor = true;
+			this.btnSend.Click += new System.EventHandler(this.OnSendClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "WinForm WebSockets";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtOutput;
+		private System.Windows.Forms.ListBox lbxInput;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button btnSend;
+	}
+}
+
---
+++ Fleck.WinFormsExample/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Fleck.WinFormsExample
+{
+	public partial class MainForm : Form
+	{
+		private readonly IList<WebSocketConnection> _allSockets = new List<WebSocketConnection>();
+
+		public MainForm()
+		{
+			InitializeComponent();
+			SetupServer();
+		}
+
+		private void SetupServer()
+		{
+			var server = new WebSocketServer("ws://localhost:54321");
+			server.Start(c =>
+				{
+					c.OnOpen = () =>
+						{
+							AddItemOnUIThread("Open!");
+							_allSockets.Add(c);
+						};
+					c.OnClose = () =>
+						{
+							AddItemOnUIThread("Closed!");
+							_allSockets.Remove(c);
+						};
+					c.OnMessage = m => AddItemOnUIThread(m);
+				});
+		}
+
+		private void AddItemOnUIThread(string message)
+		{
+			Invoke(new Action(() => lbxInput.Items.Add(message)));
+		}
+
+
+		private void OnSendClick(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(txtOutput.Text))
+				return;
+
+			foreach (WebSocketConnection socket in _allSockets)
+			{
+				socket.Send(txtOutput.Text);
+			}
+		}
+	}
+}
